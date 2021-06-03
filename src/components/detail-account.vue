02a2b30adf0561e8b828f39b1279f22086874856
judai <ernesvz@hotmail.com>
2021-06-02 23:40:26 -0500

    feat: add new fields to the profile

--- src/components/detail-account.vue
+++ src/components/detail-account.vue
@@ -1,54 +1,92 @@
 <template>
   <v-card>
-    <v-card-title>Profile</v-card-title>
+    <v-card-title class="profile">Profile</v-card-title>
       <v-spacer></v-spacer>
+      <v-row no-gutters>
+        <v-col cols="15" sm="10">
+          <div>
+            <a>
+              <v-img class="image"
+                     src="https://img.freepik.com/foto-gratis/hombre-negocios-trabajando-escritorio-oficina_79405-3705.jpg?size=626&ext=jpg"></v-img>
+            </a>
+          </div>
+        </v-col>
+        <v-col cols="10" sm="8" class="right-banner">
+          <v-card-text class="right-text" >
+            <div class="text-lg-left">Nombre: <!--{{ currentUser.firstName }} --></div>
+            <br/>
+            <div class="text-lg-left">Apellidos:<!--{{ currentUser.lastName }} --></div>
+            <br/>
+            <div class="text-lg-left">E-mail: <!--{{ currentUser.username }} --></div>
+            <br/>
+            <div class="text-lg-left">Token: <!--{{ currentUser.token }}  --></div>
+          </v-card-text>
+        </v-col>
 
-        <v-btn small color="primary" @click="navigateToEditProfile">Edit Profile</v-btn>
-        <v-dialog v-model="dialog" max-width="500px">
-          <template v-slot:activator="{on, attrs}">
-            <v-btn small color="error"  dark class="mb" v-bind="attrs" v-on="on">Deactivate Account</v-btn>
-          </template>
-          <v-card>
-            <v-card-title>
-              <span class="headline">{{ formTitle }}</span>
-            </v-card-title>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
-              <v-dialog v-model="dialog" max-width="500px">
-                <template v-slot:activator="{on, attrs}">
-                  <v-btn small color="blue darken-1"  dark class="mb" v-bind="attrs" v-on="on">Ok</v-btn>
-                </template>
-                <v-card>
-                  <v-card-title>
-                    <span class="headline">{{ formTitle1 }}</span>
-                  </v-card-title>
-                  <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn color="blue darken-1" text @click="save">Ok</v-btn>
-                  </v-card-actions>
-                </v-card>
-              </v-dialog>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
+
+        <v-btn class="button" large color="primary" @click="navigateToEditProfile">Editar Perfil</v-btn>
+            <v-dialog v-model="dialog" max-width="500px">
+              <template v-slot:activator="{on, attrs}">
+                <v-btn class="button" large color="error" v-bind="attrs" v-on="on">Deactivate Account</v-btn>
+              </template>
+              <v-card>
+                <v-card-title>
+                  <span class="headline">{{ formTitle }}</span>
+                </v-card-title>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
+                  <v-dialog v-model="dialog" max-width="500px">
+                    <template v-slot:activator="{on, attrs}">
+                      <v-btn small color="blue darken-1"  dark class="mb" v-bind="attrs" v-on="on" text @click="deleteAccount">Ok</v-btn>
+                    </template>
+                    <v-card>
+                      <v-card-title>
+                        <span class="headline">{{ formTitle1 }}</span>
+                      </v-card-title>
+                      <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="blue darken-1" text @click="save">Ok</v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </v-dialog>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+      </v-row>
   </v-card>
 </template>
 
 <script>
 
+import AccountService from "../services/accounts-service";
+
 export default {
   name: "profile",
+  props: {
+  },
   data(){
     return{
       dialog: false,
       defaultItem: {
+        id: 0,
+        firstName: '',
+        lastName: '',
+        email: '',
+        phoneNumber: '',
+        identification: '',
       },
-      displayTutorials: [],
-      tutorials: [],
+      displayProfile: [],
+      profile: [],
+      editedIndex: 1,
+      accounts: [],
     }
   },
   computed: {
+    currentUser() {
+      console.log(this.$store.state.auth.user);
+      return this.$store.state.auth.user;
+    },
     formTitle() {
       return 'Are you sure to deactivate your account?'
     },
@@ -56,7 +94,36 @@ export default {
       return 'Your account has been deactivated'
     }
   },
+  mounted() {
+    if (!this.currentUser) {
+      this.$router.push('/login');
+    }
+  },
   methods:{
+    retrieveProfile() {
+      AccountService.get()
+          .then(response => {
+            console.log(response.data);
+            this.profile = response.data;
+            this.displayProfile = response.data.map(this.getDisplayProfile);
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+    },
+    getDisplayProfile(account){
+      return {
+        firstName: account.firstName,
+        lastName: account.lastName,
+        email: account.email,
+        phoneNumber: account.phoneNumber,
+        identification: account.identification,
+      };
+    },
+    refreshList() {
+      this.retrieveProfile();
+    },
+
     close() {
       this.dialog = false
       this.$nextTick(() => {
@@ -71,11 +138,35 @@ export default {
         this.editedIndex = -1
       })
     },
+    deleteItem(item) {
+      this.editedIndex = this.displayProfile.indexOf(item);
+      this.editedItem = Object.assign({}, this.accounts[this.editedIndex]);
+      this.dialogDelete = true;
+    },
+    deleteItemConfirm() {
+      console.log(this.editedItem.id);
+      this.deleteAccount(this.editedItem.id);
+      this.accounts.splice(this.editedIndex, 1);
+      this.closeDelete();
+    },
     editItem(item) {
       this.editedIndex = this.displayTutorials.indexOf(item);
       console.log(item);
       this.editedItem = this.tutorials[this.editedIndex];
       this.dialog = true;
+    },
+    deleteAccount() {
+      AccountService.delete()
+          .then(() => {
+            this.refreshList();
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+    },
+    logout() {
+      this.$store.dispatch('auth/logout');
+      this.$router.push('/login');
     },
     navigateToEditProfile(){
       this.$router.push({name: 'edit-account'});
@@ -94,5 +185,36 @@ export default {
   font-family: 'Khula', sans-serif !important;
   font-weight: 300;
   font-size: 1.3em;
+}
+.profile{
+  margin-top: 2vw;
+  margin-left: 5vw;
+  font-size: 2vw;
+  font-family: 'Khula', sans-serif;
+}
+.image{
+  margin-top: 2vw;
+  margin-left: 5vw;
+  width: 40%;
+  height: 40%;
+  border-radius: 10px;
+}
+.button{
+  margin-top: 2vw;
+  margin-left: 6.2vw;
+  margin-bottom: 5vw;
+  border-radius: 20px;
+  width: 12%;
+  height: 12%;
+  font-size: 0.75vw;
+}
+.right-banner{
+  margin-left: 45vw;
+  margin-top: -20vw;
+  font-size: 11vw;
+}
+.right-text{
+  font-size: 1.5vw;
+  margin-top: 2vw;
 }
 </style>
