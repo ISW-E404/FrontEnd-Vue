56f5ed417efd0184d4f3cbfa5efff3db98a39777
judai <ernesvz@hotmail.com>
2021-06-19 23:49:20 -0500

    feat: adding metrics from Google Analytics to Experimtens 03

--- src/components/offices/search.vue
+++ src/components/offices/search.vue
@@ -13,7 +13,25 @@
         {{item.title}}
       </v-list-item>
     </v-list>
+
+    <v-card>
+      <v-spacer></v-spacer>
+      <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Buscar por precio"
+          single-line
+          hide-details
+      ></v-text-field>
+      <v-list tile="prueba">
+        <v-list-item  v-for="item in itemsPrice" :key="item.title" :to="item.link" link>
+          {{item.title}}
+        </v-list-item>
+      </v-list>
+    </v-card>
   </v-card>
+
+
 
 </template>
 
@@ -37,6 +55,21 @@ name: "search",
         {
           title: "San Isidro"
         },
+      ],
+      itemsPrice: [
+
+        {
+          title: "S/ 0.00 - S/ 100.00"
+        },
+        {
+          title: "S/ 100.00 - S/ 250.00"
+        },
+        {
+          title: "S/ 250.00 - S/ 500.00"
+        },
+        {
+          title: "S/ 500.00 - S/ 1000.00"
+        }
       ]
     }
   }
